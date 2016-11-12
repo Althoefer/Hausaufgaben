45b2f26d46f0befef15396bb8150939447b6ca3f
Tom Althoefer <althoefer@cats.rwth-aachen.de>
2016-11-12 16:53:29 +0100

    homework 5 is now available for testing and correcting

---
+++ src/Quadratwurzel.java
@@ -0,0 +1,15 @@
+/**
+ * implements the interface Rechenoperatio 
+ *
+ */
+public class Quadratwurzel implements Rechenoperation {
+
+	/**
+	 * returns the square root of parameter x
+	 */
+	@Override
+	public double berechne(double x) {
+		return Math.sqrt(x);
+	}
+
+}
