d2a67722aa8cd48b156a8233333457fececa2471
Tom Althoefer <tom.linus98@web.de>
2016-12-10 16:24:00 +0100

    some more comments

--- ha09/Knight.java
+++ ha09/Knight.java
@@ -10,15 +10,28 @@ import java.util.ArrayList;
  */
 public class Knight extends Chessman{
 
+	/**
+	 * constructs a new Knight, uses the constructer of the super class
+	 * @param p
+	 * 		Position of the Knight
+	 */
 	public Knight(Position p) {
 		super(p);
 	}
 
+	/**
+	 * @return
+	 * 		ArrayList with all possbile Positions, the Knight can move to
+	 */
 	@Override
 	public ArrayList<Position> getMoveList() {
+		//temporary save the Position to avoid some methodcalls
 		int x = this.getPosition().getX();
 		int y = this.getPosition().getY();
 		ArrayList<Position> moveList = new ArrayList<Position>();
+		
+		//add all possible Positions to the moveList
+		//Positions are not checked yet
 		moveList.add(new Position(x+2, y+1));
 		moveList.add(new Position(x+2, y-1));
 		moveList.add(new Position(x-2, y+1));
@@ -28,9 +41,11 @@ public class Knight extends Chessman{
 		moveList.add(new Position(x-1, y+2));
 		moveList.add(new Position(x-1, y-2));
 		
+		//go through the list and check all Positions
+		//delete if !isValid()
 		for(int i = 0; i<moveList.size(); i++){
 			if(moveList.get(i).isValid()){
-				
+				//everything allright
 			}else{
 				moveList.remove(i);
 				//else we might skip a element the list, because the element at i ist replaced by a new one
@@ -40,6 +55,10 @@ public class Knight extends Chessman{
 		return moveList;
 	}
 	
+	/**
+	 * @return
+	 * 		Position of the Knight "Springer: (x/y)"
+	 */
 	public String toString(){
 		return "Springer: (" + this.getPosition().toString() + ")";
 	}
--- ha09/Rook.java
+++ ha09/Rook.java
@@ -11,20 +11,32 @@ import java.util.ArrayList;
  */
 public class Rook extends Chessman {
 
+	/**
+	 * constructs a new Rook, uses the constructer of the super class
+	 * @param p
+	 * 		Position of the Rook
+	 */
 	public Rook(Position p) {
 		super(p);
 	}
 
+	/**
+	 * @return
+	 * 		ArrayList with all possbile Positions, the Rook can move to
+	 */
 	@Override
 	public ArrayList<Position> getMoveList() {
+		//temporary save the Position to avoid some methodcalls
 		int x = this.getPosition().getX();
 		int y = this.getPosition().getY();
 		ArrayList<Position> moveList = new ArrayList<Position>();
+		//add all Positions with the same x-coordinate
 		for(int i = 1; i<=8; i++){
 			if(i != y){
 				moveList.add(new Position(x, i));
 			}
 		}
+		//add all Positions with the same x-coordinate
 		for(int i = 1; i<=8; i++){
 			if(i !=x){
 				moveList.add(new Position(i, y));
@@ -33,6 +45,10 @@ public class Rook extends Chessman {
 		return moveList;
 	}
 
+	/**
+	 * @return
+	 * 		Position of the Knight "Springer: (x/y)"
+	 */
 	public String toString() {
 		return "Turm: (" + this.getPosition().toString() + ")";
 	}
