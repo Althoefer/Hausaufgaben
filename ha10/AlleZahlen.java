f2af5efe7349c28a516f47708c774f4e33580312
Marco Schlicht <marcoschlicht@onlinehome.de>
2016-12-16 12:39:13 +0100

    Anti Golf

--- ha10/AlleZahlen.java
+++ ha10/AlleZahlen.java
@@ -1,10 +1,10 @@
 package ha10;
 
 public class AlleZahlen {
-	public static void main(String[] args){
-		for(int i=1; i<10000; i++){
-			Zahlwort zw = new ZahlwortDeutsch(i);
-			System.out.println(zw.toString());
+	public static void main(String[] args) {
+		for (int i = 1; i < 1000; i++) {
+			Zahlwort zw = new ZahlwortDeutschAlternative(i);
+			System.out.println(zw.getWortString());
 		}
 	}
 }
