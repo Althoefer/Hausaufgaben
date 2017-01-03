4711bef7488e8ac6e18bf2d39c8a19bf905d2121
C. Wassermann <chris@Christians-MacBook-Pro.local>
2017-01-03 11:07:33 +0100

    No logic changes, just cosmetics
    did not look into the logic yet, but made the code and the output more
    beautiful.
    If anyone has a meaningful java doc comment for the function pls add

--- ha11/Kockkurve.java
+++ ha11/Kockkurve.java
@@ -1,43 +1,48 @@
+
 package ha11;
 
-import java.awt.Point;
-
 public class Kockkurve {
-	
+
 	/**
-	 * 
-	 * @param ax, ay	Koordinaten des Startpunktes
-	 * @param bx, by	Koordinaten des Endpunktes
-	 * @param rek		Tiefe der Rekursion
+	 * @param ax
+	 *            x-Koordinate des Startpunktes
+	 * @param ay
+	 *            y-Koordinate des Startpunktes
+	 * @param bx
+	 *            x-Koordinate des Endpunktes
+	 * @param by
+	 *            y-Koordinate des Endpunktes
+	 * @param rek
+	 *            Tiefe der Rekursion
 	 */
-	public static void kochKurve(double ax, double ay, double bx, double by, double rek){
-		//wen die Rekursionstiefe 0 beträgt, wird der Anfangspunkt ausgegeben
-		if(((int) rek) == 0){
-			System.out.printf("(%.1f/%.1f) ", ax, ay);
+	public static void kochKurve(double ax, double ay, double bx, double by, double rek) {
+		// wenn die Rekursionstiefe 0 beträgt, wird der Anfangspunkt ausgegeben
+		if (((int) rek) == 0) {
+			System.out.printf("(%.1f/%.1f)\n", ax, ay);
 		} else {
+			// Rekursionstiefe beim nächsten Aufruf um eins verringern
 			rek--;
-			
-			//erster Punkt
-			double x1 = ax + (bx-ax)/3 ;
-			double y1 = ay + (by-ay)/3 ;
-			//zweiter Punkt
-			double x2 = ax + (bx-ax)/2 + (Math.sqrt(3)/2) * ((by-ay)/3);
-			double y2 = ay + (by-ay)/2 - (Math.sqrt(3)/2) * ((bx-ax)/3);
-			//dritter Punkt
-			double x3 = bx - (bx-ax)/3 ;
-			double y3 = by - (by-ay)/3 ;
-			
+			// erster Punkt
+			double x1 = ax + (bx - ax) / 3;
+			double y1 = ay + (by - ay) / 3;
+			// zweiter Punkt
+			double x2 = ax + (bx - ax) / 2 + (Math.sqrt(3) / 2) * ((by - ay) / 3);
+			double y2 = ay + (by - ay) / 2 - (Math.sqrt(3) / 2) * ((bx - ax) / 3);
+			// dritter Punkt
+			double x3 = bx - (bx - ax) / 3;
+			double y3 = by - (by - ay) / 3;
+			// rekursive Aufrufe
 			kochKurve(ax, ay, x1, y1, rek);
 			kochKurve(x1, y1, x2, y2, rek);
 			kochKurve(x2, y2, x3, y3, rek);
 			kochKurve(x3, y3, bx, by, rek);
-			System.out.println();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		kochKurve(0, 500, 500, 500, 2);
-		//da immer nur die Anfangspunkte ausgegeben wird, muss am ende noch der Endpunkt ausgegeben werden
-		System.out.print("(500/500)");
+		// da immer nur die Anfangspunkte ausgegeben werden, muss am Ende noch
+		// der Endpunkt ausgegeben werden
+		System.out.print("(500,0/500,0)");
 	}
 }
